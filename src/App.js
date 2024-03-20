ad5328d6eac8a0b85e1d77a04530197b1e0cd986
asstaff2020 <asstaff2020@gmail.com>
2024-03-20 16:56:58 +0900

    초기 파일구조 세팅

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+// import styled from 'styled-components';
+
+
+function App() {
+  return (
+    <div className="App">
+      TITLE123
+    </div>
+  );
+}
+
+export default App;
