e5ed69c285297c0647096e3e35bc0d71206658ff
asstaff2020 <asstaff2020@gmail.com>
2024-03-21 12:06:59 +0900

    FormInput 포커싱 스타일

--- src/components/form/FormInput.js
+++ src/components/form/FormInput.js
@@ -1,28 +1,46 @@
+import { useState } from "react";
 import styled from "styled-components";
 
 const Wrap = styled.div`
-    input{
-        width: 90%;
-        height: 57px;
-        border-radius: 16px;
-        border: 1px solid #DFDFDF;
-        padding: 18px 23px;
-        color: #333333;
-        font-size: 16px;
-    }
+  input {
+    width: 90%;
+    height: 57px;
+    border-radius: 16px;
+    border: 1px solid #dfdfdf;
+    padding: 18px 23px;
+    color: #333333;
+    font-size: 16px;
+  }
 
-    input::placeholder{
-        color: #909090;
-        font-size: 16px;
-    }
-` 
+  input::placeholder {
+    color: #909090;
+    font-size: 16px;
+  }
 
-const FormInput = ({title, type, placeholder}) => {
-    return(
-        <Wrap>
-            <p>{title}</p>
-            <input type={type} placeholder={placeholder} />
-        </Wrap>
-    );
-}
+  input:focus {
+    border: 1px solid #33C2FF;
+    outline: none !important;
+  }
+
+  p {
+    color: ${(props) => (props.isFocused ? "#33C2FF" : "#333333")};
+    margin-bottom: 10px;
+  }
+`;
+
+const FormInput = ({ title, type, placeholder }) => {
+  const [isFocused, setIsFocused] = useState(false);
+
+  return (
+    <Wrap isFocused={isFocused}>
+      <p>{title}</p>
+      <input
+        type={type}
+        placeholder={placeholder}
+        onFocus={() => setIsFocused(true)}
+        onBlur={() => setIsFocused(false)}
+      />
+    </Wrap>
+  );
+};
 export default FormInput;
