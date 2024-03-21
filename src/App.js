7c132d2673f77c0c2dc059d80c23aed443a4fc95
kr2074822 <rladudwn0215@naver.com>
2024-03-21 12:19:59 +0900

    app 헤더 적용

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import './App.css';
 import "./global/font.css";
 import Join from './components/join/Join';
 import Footer from './components/layout/Footer';
+import Header from './components/layout/Header';
 // import styled from 'styled-components';
 
 
 function App() {
   return (
     <div className="App">
+      <Header />
       TITLE123
-    <Join />
-    <Footer />
+      <Join />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/layout/Header.js
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+
+// footer link icon
+import telegram from '../../assets/images/footer/telegram_icon.png';
+import discord from '../../assets/images/footer/discord_icon.png';
+import instagram from '../../assets/images/footer/instagram_icon.png';
+import twitter from '../../assets/images/footer/twitter_icon.png';
+import kakao from '../../assets/images/footer/kakao_icon.png';
+
+// footer menu icon
+import home from '../../assets/images/common/home_icon.png';
+import product from '../../assets/images/common/product_icon.png';
+import cart from '../../assets/images/common/cart_icon.png';
+import user from '../../assets/images/common/user_icon.png';
+
+import homeOn from '../../assets/images/common/home_on_icon.png';
+import product_icon from '../../assets/images/common/product_on_icon.png';
+import cartOnproduct_icon from '../../assets/images/common/cart_on_icon.png';
+import userOnproduct_icon from '../../assets/images/common/user_on_icon.png';
+
+const Wrapper = styled.div`
+    padding: 22px 16px 60px;
+    background-color: #F5F5F5;
+`;
+
+
+function Header() {
+    return (
+        <Wrapper>
+            111111111111
+        </Wrapper>
+    );
+}
+
+export default Header;
