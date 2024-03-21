3e3fd3c9ca68ea043a2a02f97daf593be3111b04
asstaff2020 <asstaff2020@gmail.com>
2024-03-21 17:01:42 +0900

    체크박스 체크 스타일

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ function App() {
   return (
     <div className="App">
       <Header type="t4" />
-      TITLE123
       <Join />
       <Footer />
     </div>
--- src/components/form/FormCheckBox.js
+++ src/components/form/FormCheckBox.js
@@ -2,45 +2,53 @@ import styled from "styled-components";
 import checkImg from "../../assets/images/join/check.png";
 
 const Wrap = styled.label`
+  display: flex;
+  align-items: center;
 
-    input[type="checkbox"]{
-        display: none;
-    }
+  input[type="checkbox"] {
+    display: none;
+  }
 
-    input[type="checkbox"] + label{
-        display: inline-block;
-        width: 11px;
-        height: 11px;
-        border: 1px solid #33C2FF;
-        position: relative;
-    }
+  input[type="checkbox"] + label {
+    display: inline-block;
+    width: 14px;
+    height: 14px;
+    border: 1px solid #33c2ff;
+    position: relative;
+  }
 
-    input[id="loginState"]:checked + label::after{
-        content: '';
-        background-image: url(checkImg);
-        font-size: 11px;
-        width: 11px;
-        height: 11px;
-        text-align: center;
-        position: absolute;
-        left: 0;
-        top:0;
-      }
+  input[id="loginState"]:checked + label::after {
+    content: "";
+    background-image: url(${checkImg});
+    background-size: cover;
+    width: 14px;
+    height: 11px;
+    text-align: center;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    object-fit: cover;
+  }
 
-    span{
-        font-size: 14px;
-        margin-left: 6px;
-    }
+  span {
+    font-size: 14px;
+    margin-left: 6px;
+  }
 `;
 
-const FormCheckBox = () =>{
-    return(
-        <Wrap for="loginState">
-            <input id="loginState" type="checkbox" />
-            <label for="loginState"></label>
-            <span>로그인 상태 유지</span>
-        </Wrap>
-    );
-}
+const FormCheckBox = () => {
+  return (
+    <Wrap htmlForfor="loginState">
+      <input id="loginState" type="checkbox" />
+      <label
+        className="checkbox__label"
+        img={checkImg}
+        htmlFor="loginState"
+      ></label>
+      <span>로그인 상태 유지</span>
+    </Wrap>
+  );
+};
 
 export default FormCheckBox;
