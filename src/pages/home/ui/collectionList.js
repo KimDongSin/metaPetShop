15f6e3720712f8658e4d2bc1bf59045c5b6431cb
kr2074822 <rladudwn0215@naver.com>
2024-03-23 01:41:32 +0900

    collection 컴포넌트 분리

---
+++ src/pages/home/ui/collectionList.js
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+import dog1 from "../../../assets/images/common/dog_sample1.png"
+import dog2 from "../../../assets/images/common/dog_sample2.png"
+import dog3 from "../../../assets/images/common/dog_sample3.png"
+import Tag from "./tag";
+
+
+const Wrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: flex-start;
+    flex-direction: column;
+    margin-bottom: 8px;
+    padding: 12px;
+    border-radius: 21px;
+    background: #FFF;
+    box-shadow: 0 4px 10px 0 #0000000F;
+
+    img {
+        width: 164px;
+        height: 164px;
+        object-fit: cover;
+        border-radius: 24px;
+        margin-bottom: 14px;
+    }
+
+    & > span {
+        font-size: 18px;
+        font-weight: 500;
+        line-height: 23px;
+        color: #333;
+    }
+`;
+
+function CollectionList() {
+    return (
+        <Wrapper>
+            <img src={dog2} />
+            <span>good monkey</span>
+            <Tag />
+        </Wrapper>
+    )
+}
+
+export default CollectionList;
---
+++ src/pages/home/ui/tag.js
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+    margin-top: 15px;
+
+    span {
+        display: inline-block;
+        padding: 5px 7px;
+        margin-right: 4px;
+        font-size: 14px;
+        font-weight: 500;
+        line-height: 18px;
+        border-radius: 6px;
+
+        &:first-child {
+            color: #33C2FF;
+            background: #EDF8FF;
+        }
+
+        &:last-child {
+            color: #FEC600;
+            background: #FFF7DC;
+        }
+    }
+`;
+
+function Tag() {
+    return (
+        <Wrapper>
+            <span>550MET</span>
+            <span>current Bid</span>
+        </Wrapper>
+    )
+}
+
+export default Tag;
