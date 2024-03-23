020a3fa7328fa86ae561b440165b0c35a2431480
asstaff2020 <asstaff2020@gmail.com>
2024-03-23 12:34:14 +0900

    라우터 설정변경

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import "./global/font.css";
-import Join from './pages/join/Join';
 import Footer from './components/layout/Footer';
 import Header from './components/layout/Header';
 import Home from './pages/home/home';
@@ -12,11 +11,8 @@ function App() {
   return (
     <div className="App">
       <Header type="t4" />
-      <Home />
-      <Join />
-      <Footer />
-
       <MainRouter />
+      <Footer />
     </div>
   );
 }
--- src/common/routers/index.js
+++ src/common/routers/index.js
@@ -1,12 +1,13 @@
 import { Suspense } from "react";
 import { Route, Routes } from "react-router";
 import Join from "../../pages/join/Join";
+import Home from "../../pages/home/home";
 
 const MainRouter = () => {
     return(
         <Suspense fallback={<div />}>
             <Routes>
-                <Route path="/" element={<></>} />
+                <Route path="/" element={<Home />} />
                 <Route path="/login" element={<Join />} />
             </Routes>
         </Suspense>
