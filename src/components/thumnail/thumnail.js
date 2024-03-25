0346a70a4ad824abd1971a7aa4352a86a46faf6f
kr2074822 <rladudwn0215@naver.com>
2024-03-25 21:33:13 +0900

    detail 작업중

--- src/components/thumnail/thumnail.js
+++ src/components/thumnail/thumnail.js
@@ -1,7 +1,6 @@
 import styled from "styled-components";
 import { Swiper, SwiperSlide } from 'swiper/react';
 import sampleImg from '../../assets/images/common/dog_sample3.png';
-import 'swiper/css';
 import Tag from "../../pages/home/ui/tag";
 
 const Wrapper = styled.div`
@@ -9,18 +8,19 @@ const Wrapper = styled.div`
 `;
 
 const ItemImg = styled.div`
-    padding: 12px 12px 0;
+    padding: 12px 0 0;
 
     & > img {
         object-fit: cover;
         width: 100%;
         height: 155px;
+        height: ${(props) => (props.size == 'large' ? '382px' : '155px')};
         border-radius: 16px;
     }
 `;
 
 const ItemInfo = styled.div`
-    padding: 20px 16px 22px;
+    padding: 20px 0 22px;
     display: flex;
     justify-content: space-between;
     align-items: center;
@@ -34,13 +34,24 @@ const ItemTitle = styled.div`
         line-height: 23px;
         color: #333333;
     }
+
+    & > p {
+        margin-top: 10px;
+        padding: 11px 14px;
+        font-size: 14px;
+        font-weight: 400;
+        line-height: 18px;
+        color: #333333;
+        background: #F9F9F9;
+        border-radius: 10px;
+    }
 `;
 
-function ItemThumbnail() {
+function ItemThumbnail({ size }) {
 
     return (
         <Wrapper>
-            <ItemImg>
+            <ItemImg size={size}>
                 <img src={sampleImg} />
             </ItemImg>
 
@@ -48,6 +59,9 @@ function ItemThumbnail() {
                 <ItemTitle>
                     <span>good monkey</span>
                     <Tag />
+                    <p>
+                        good monkeygood monkeygood monkeygood monkeygood monkey
+                    </p>
                 </ItemTitle>
             </ItemInfo>
         </Wrapper>
--- src/pages/detail/detail.js
+++ src/pages/detail/detail.js
@@ -1,22 +1,50 @@
 import styled from "styled-components";
 // import sampleImg from '../../../assets/images/common/dog_sample3.png';
 import ItemThumbnail from "../../components/thumnail/thumnail";
+import sampleUser from '../../assets/images/common/user_img.png';
+import shareIcon from '../../assets/images/common/share_icon.png';
+import { Link } from "react-router-dom";
 
 const Wrapper = styled.div`
-    padding: 18px 16px;
+    padding: 0px 16px;
 
 `;
 
 const User = styled.div`
-
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
 `;
 
-const UserInfo = styled.div`
+const UserInfo = styled(Link)`
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+
+    & > img {
+        width: 40px;
+        height: 40px;
+        margin-right: 13px;
+    }
+
+    & > span {
+        font-size: 18px;
+        font-weight: 600;
+        color: #333333;
+    }
 
 `;
 
 const Share = styled.div`
+    & > button {
+        background: none;
+        border: none;
 
+        & > img {
+            width: 22px;
+            height: 22px;
+        }
+    }
 `;
 
 const ItemDetail = styled.div`
@@ -28,30 +56,16 @@ function Detail() {
         <Wrapper>
             <User>
                 <UserInfo>
-                    <img />
+                    <img src={sampleUser} />
                     <span>good monkey</span>
                 </UserInfo>
                 <Share>
-                    <button><img /></button>
+                    <button><img src={shareIcon} /></button>
                 </Share>
             </User>
 
             <ItemDetail>
-
-                <ItemThumbnail></ItemThumbnail>
-                {/* <ItemImg>
-                    <img src={sampleImg} />
-                </ItemImg>
-
-                <ItemInfo>
-                    <ItemTitle>
-                        <span>TITLE 123</span>
-                        <Tag />
-                        <p>
-                            good monkeygood monkeygood monkeygood monkeygood monkey
-                        </p>
-                    </ItemTitle>
-                </ItemInfo> */}
+                <ItemThumbnail size='large'></ItemThumbnail>
             </ItemDetail>
         </Wrapper>
     )
