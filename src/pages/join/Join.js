320cf51c4c25b4ea1871a96ca3cb605a7cf18d7b
asstaff2020 <asstaff2020@gmail.com>
2024-03-27 01:38:21 +0900

    회원가입 계정만들기 페이지

--- src/pages/join/Join.js
+++ src/pages/join/Join.js
@@ -1,8 +1,18 @@
 import styled from "styled-components";
 import FormInput from "../../components/form/FormInput";
+import { LoginBtn } from "../../components/styled/UI/button/Button";
 
 const Wrap = styled.div`
     padding: 0 28px;
+    
+
+    & > div:not(:last-child){
+        margin-bottom: 20px;
+    }
+
+    & .next__btn{
+        margin: 352px 0 46px;
+    }
 `;
 
 const Join = () => {
@@ -11,6 +21,8 @@ const Join = () => {
             <FormInput title="이메일">이메일</FormInput>
             <FormInput title="비밀번호">비밀번호</FormInput>
             <FormInput title="비밀번호 확인">비밀번호 확인</FormInput>
+
+            <LoginBtn className="next__btn">다음</LoginBtn>
         </Wrap>
     );
 }
