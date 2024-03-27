d5b570b00e97132b8a48ac8d8db651ee909312ac
asstaff2020 <asstaff2020@gmail.com>
2024-03-27 15:33:27 +0900

    JoinDone 컴포넌트 버튼

--- src/pages/join/JoinDone.js
+++ src/pages/join/JoinDone.js
@@ -1,43 +1,50 @@
 import styled from "styled-components";
 import JoinImg from "../../assets/images/join/JoinDone.png";
 import { LoginBtn } from "../../components/styled/UI/button/Button";
+import { Link } from "react-router-dom";
 
 const Wrap = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    padding: 97px 28px 46px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 97px 28px 46px;
 
-    & > img{
-        margin-bottom: 41px;
-    }
+  & > img {
+    margin-bottom: 41px;
+  }
 
-    & > p{
-        color: #333333;
-        font-size: 18px;
-        font-weight: 500;
-        margin-bottom: 12px;
-    }
+  & > p {
+    color: #333333;
+    font-size: 18px;
+    font-weight: 500;
+    margin-bottom: 12px;
+  }
 
-    & > span{
-        color: #565656;
-        font-size: 14px;
-    }
+  & > span {
+    color: #565656;
+    font-size: 14px;
+  }
 
-    & .join__done__loginbtn{
-        margin-top: 423px;
-    }
-`
+  & .join__done__loginbtn {
+    margin-top: 423px;
+  }
+`;
+
+const JoinDoneLink = styled(Link)`
+    width: 100%;
+`;
 
 const JoinDone = () => {
-    return (
-        <Wrap>
-            <img src={JoinImg} />
-            <p>회원가입을 완료하였습니다.</p>
-            <span>로그인 후 원활한 서비스를 이용해주세요.</span>
+  return (
+    <Wrap>
+      <img src={JoinImg} />
+      <p>회원가입을 완료하였습니다.</p>
+      <span>로그인 후 원활한 서비스를 이용해주세요.</span>
 
-            <LoginBtn className="join__done__loginbtn">회원가입 완료</LoginBtn>
-        </Wrap>
-    );
-}
+      <JoinDoneLink to="/">
+        <LoginBtn className="join__done__loginbtn">회원가입 완료</LoginBtn>
+      </JoinDoneLink>
+    </Wrap>
+  );
+};
 export default JoinDone;
