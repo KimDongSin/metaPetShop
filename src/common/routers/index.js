14b84e523cb0cae86a63b01ab2099ff9eac37ceb
KimDongSin <asstaff2020@gmail.com>
2024-04-05 16:42:09 +0900

    컨택페이지 라우트 추가

--- src/common/routers/index.js
+++ src/common/routers/index.js
@@ -10,25 +10,27 @@ import InfoFind from "../../pages/InfoFind/InfoFind";
 import IdFind from "../../pages/InfoFind/IdFind";
 import PwdFind from "../../pages/InfoFind/PwdFind";
 import Leave from "../../pages/leave/Leave";
+import Contact from "../../pages/contact/Contact";
 
 const MainRouter = () => {
-    return(
-        <Suspense fallback={<div />}>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/join" element={<Join />} />
-                <Route path="/join/joinDoen" element={<JoinDone />} />
-                <Route path="/detail" element={<Detail />} />
-                <Route path="/*" element={<NotFound />} />
-                <Route path="/infoFind" element={<InfoFind />} />
-                <Route path="/idFind" element={<IdFind />} />
-                <Route path="/pwdFind" element={<PwdFind />} />
-                {/* 회원탈퇴 페이지 */}
-                <Route path="/leave" element={<Leave />} />
-            </Routes>
-        </Suspense>
-    );
-}
+  return (
+    <Suspense fallback={<div />}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/join" element={<Join />} />
+        <Route path="/join/joinDoen" element={<JoinDone />} />
+        <Route path="/detail" element={<Detail />} />
+        <Route path="/*" element={<NotFound />} />
+        <Route path="/infoFind" element={<InfoFind />} />
+        <Route path="/idFind" element={<IdFind />} />
+        <Route path="/pwdFind" element={<PwdFind />} />
+        {/* 회원탈퇴 페이지 */}
+        <Route path="/leave" element={<Leave />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Suspense>
+  );
+};
 
 export default MainRouter;
