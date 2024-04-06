b3f7030485d6793b63f92334abd424e04f46bdbd
KimDongSin <asstaff2020@gmail.com>
2024-04-06 18:48:15 +0900

    구매목록 date

--- src/pages/myPage/PurchaseList.js
+++ src/pages/myPage/PurchaseList.js
@@ -1,11 +1,33 @@
 import styled from "styled-components";
 
-const Wrap = styled.div``;
+const Wrap = styled.div`
+  .purchease__list__wrap {
+    padding: 18px 28px;
+
+    .date__text {
+      font-size: 14px;
+      font-weight: 500;
+      color: #909090;
+    }
+  }
+
+  & .purchease__list__wrap:not(:first-child) {
+    border: 4px solid #f9f9f9;
+  }
+`;
 
 const PurchaseList = () => {
   return (
     <Wrap>
-      <div></div>
+      <div className="purchease__list__wrap">
+        <p className="date__text">6월 20일</p>
+
+        <div className="list__wrap__item">
+          <div className="list__itme__box"></div>
+
+          <div className="list__btn__box"></div>
+        </div>
+      </div>
     </Wrap>
   );
 };
