2afd99edd5724ad7029dbfa875ee416e613183c9
kr2074822 <rladudwn0215@naver.com>
2024-04-15 17:16:22 +0900

    파이어베이스 로그인 설정 추가

--- src/common/api/firebase.js
+++ src/common/api/firebase.js
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getDatabase } from "firebase/database"; // 추가
+import {getAuth} from 'firebase/auth'
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -23,3 +24,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 export const db = getDatabase(app); // 추가
+export const authService = getAuth()
